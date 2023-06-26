7e8091d1aef433dbcbfdb77d6fb7bd8553dd5f92
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-06-26 12:09:21 +0530

    Refractor: Change the logic for buying books and set the quantity of books and discount while adding books

--- src/test/java/com/sdb/tdd/CalculateDiscountTest.java
+++ src/test/java/com/sdb/tdd/CalculateDiscountTest.java
@@ -14,7 +14,7 @@ import com.sdb.entity.SoftwareDevelopmentBookSetDiscount;
 
 public class CalculateDiscountTest {
 
-	CalculateDiscount calculateDiscount = new CalculateDiscount();
+	CalculateDiscount calculateDiscount;;
 
 	@BeforeEach
 	public void setup() {
@@ -23,6 +23,7 @@ public class CalculateDiscountTest {
 		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(3, 10));
 		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(4, 20));
 		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(5, 25));
+		calculateDiscount = new CalculateDiscount(byDifferentCopiesDiscountList);
 	}
 
 	@Test
@@ -32,38 +33,29 @@ public class CalculateDiscountTest {
 
 	@Test
 	public void buyingOneBook() {
-		List<SoftwareDevelopmentBook> sdbList = new ArrayList<>();
 		SoftwareDevelopmentBook sdbFirstI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIBook();
-		sdbList.add(sdbFirstI);
-		double discount = 0.00;
-		int noOfBooks = 1;
-		assertEquals(50.0, calculateDiscount.getTotalPrice(discount, noOfBooks));
+		calculateDiscount.Add(sdbFirstI);
+		assertEquals(50.0, calculateDiscount.getTotalPrice());
 	}
 
 	@Test
 	public void buyingTwoCopiesOfDifferentBook() {
-		List<SoftwareDevelopmentBook> sdbList = new ArrayList<>();
 		SoftwareDevelopmentBook sdbFirstI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIBook();
 		SoftwareDevelopmentBook sdbSecondI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIIBook();
-		double discount = 5;
-		sdbList.add(sdbFirstI);
-		sdbList.add(sdbSecondI);
-		int noOfBooks = sdbList.size();
-		assertEquals(95.0, calculateDiscount.getTotalPrice(discount, noOfBooks));
+		calculateDiscount.Add(sdbFirstI);
+		calculateDiscount.Add(sdbSecondI);
+		assertEquals(95.0, calculateDiscount.getTotalPrice());
 	}
 
 	@Test
 	public void buyingThreeCopiesOfDifferentBook() {
-		List<SoftwareDevelopmentBook> sdbList = new ArrayList<>();
 		SoftwareDevelopmentBook sdbFirstI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIBook();
 		SoftwareDevelopmentBook sdbSecondI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIIBook();
 		SoftwareDevelopmentBook sdbThirdI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIIIBook();
-		double discount = 10;
-		sdbList.add(sdbFirstI);
-		sdbList.add(sdbSecondI);
-		sdbList.add(sdbThirdI);
-		int noOfBooks = sdbList.size();
-		assertEquals(135, calculateDiscount.getTotalPrice(discount, noOfBooks));
+		calculateDiscount.Add(sdbFirstI);
+		calculateDiscount.Add(sdbSecondI);
+		calculateDiscount.Add(sdbThirdI);
+		assertEquals(135, calculateDiscount.getTotalPrice());
 
 	}
 
