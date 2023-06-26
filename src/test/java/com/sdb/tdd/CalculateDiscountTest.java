14738198c834e64424e3467bc856f7ff34a6db7f
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-06-26 10:23:05 +0530

    Add:Defined all books name

--- src/test/java/com/sdb/tdd/CalculateDiscountTest.java
+++ src/test/java/com/sdb/tdd/CalculateDiscountTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 public class CalculateDiscountTest {
 
 	public static final String SOFTWARE_DEVELOPMENT_BOOK_I = "Clean Code (Robert Martin, 2008)";
+	public static final String SOFTWARE_DEVELOPMENT_BOOK_II = "The Clean Coder (Robert Martin, 2011)";
+	public static final String SOFTWARE_DEVELOPMENT_BOOK_III = "Clean Architecture (Robert Martin, 2017)";
+	public static final String SOFTWARE_DEVELOPMENT_BOOK_IV = "Test Driven Development by Example (Kent Beck, 2003)";
+	public static final String SOFTWARE_DEVELOPMENT_BOOK_V = "Working Effectively With Legacy Code (Michael C. Feathers, 2004)";
 
 	@Test
 	public void initializeToBuyBook() {
