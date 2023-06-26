0ae8be3addd2252cf2e256684245a9d90e04d2e6
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-06-26 10:21:14 +0530

    Refractor: Extract book names from string literal

--- src/test/java/com/sdb/tdd/CalculateDiscountTest.java
+++ src/test/java/com/sdb/tdd/CalculateDiscountTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 
 public class CalculateDiscountTest {
 
+	public static final String SOFTWARE_DEVELOPMENT_BOOK_I = "Clean Code (Robert Martin, 2008)";
+
 	@Test
 	public void initializeToBuyBook() {
 		CalculateDiscount calculateDiscount = new CalculateDiscount();
@@ -16,7 +18,7 @@ public class CalculateDiscountTest {
 	@Test
 	public void buyingOneBook() {
 		CalculateDiscount calculateDiscount = new CalculateDiscount();
-		String book = "Clean Code (Robert Martin, 2008)";
+		String book = SOFTWARE_DEVELOPMENT_BOOK_I;
 		double discount = 0.00;
 		int noOfBooks = 1;
 		assertEquals(50.0, calculateDiscount.getTotalPrice(discount, noOfBooks));
